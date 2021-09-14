c5312503002554a875ac5948d1b4513bb0d4e18d
thephotogenicbug <navinkumarachari@gmail.com>
2021-09-14 12:20:54 +0530

    MERN : Backend : Encrypt user password using bcrypt

--- models/userModel.js
+++ models/userModel.js
@@ -33,6 +33,20 @@ const userSchema = mongoose.Schema(
   }
 );
 
+// encrypt user password using bcrypt
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+// decrypt user password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
