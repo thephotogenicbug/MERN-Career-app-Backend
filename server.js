d3e1f7e4bb894e26a5f831a289190dfe7a2b0b9b
thephotogenicbug <navinkumarachari@gmail.com>
2021-09-14 11:59:57 +0530

    MERN : Backend : add userRoutes userControllers and connect to MongoDB Database

--- server.js
+++ server.js
@@ -7,13 +7,16 @@ app.use(cors());
 dotenv.config();
 app.use(express.json());
 
+const connectDB = require("./config/db");
+connectDB();
 
 // get request
 app.get("/", (req, res) => {
   res.send("api is running...");
 });
 
-
+const userRoutes = require("./routes/userRoutes");
+app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
